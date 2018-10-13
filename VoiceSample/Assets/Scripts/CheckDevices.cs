ab65e138a32581d78a21cc09bda0e4da7fbe6109
pollenjp <polleninjp@gmail.com>
2018-10-13 18:42:12 +0900

    Add VoiceSample/

---
+++ VoiceSample/Assets/Scripts/CheckDevices.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckDevices : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // ref: https://docs.unity3d.com/ScriptReference/Microphone-devices.html
+        foreach (string device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
